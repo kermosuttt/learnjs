070fb26f8cf57666d877e543636a2ab23b17f63c
Kermo Sutt <kermo.sutt@tptlive.ee>
2023-09-18 11:28:52 +0300

    New update on js

--- script.js
+++ script.js
@@ -1,67 +1,72 @@
-let num = 10;
-
-if(num > 10) {
-    console.log('Suurem');
-} else if (num === 10) {
-    console.log('Võrdne');
-} else {
-    console.log('Väiksem');
+function hello(name='Nimetu', age=Infinity){
+    let yearText = 'years';
+    if(age === 1 || age === -1){
+        yearText = 'year';
+    }
+    //console.log(' hello ' + name + ' and you are ' + age + ' years old');
+    console.log(`hello ${name} and you are ${age} ${yearText} old`);
 }
 
-let day = 11;
+hello('Kermo', 17);
+hello('Sutt', -1);
+hello();
 
-switch(day){
-    case 0:
-        console.log('Esmaspäev');
-        break;
-    case 1:
-        console.log('Teisipäev');
-        break;
-    case 2:
-        console.log('Kolmapäev');
-        break;
-    case 3:
-        console.log('Neljapäev');
-        break;
-    case 4:
-        console.log('Reede');
-        break;
-    case 5:
-    case 6:
-        console.log('Nädalavahetus');
-        break;
-    default:
-        console.log('Imelik päev');
-        break;
+function volume(h, l, w){
+    return h*w*l;
 }
 
-for(let i=10;i>1;i-=2){
+let answer = volume(4,2,5);
+console.log(answer);
+
+function numbers(i) {
     console.log(i);
+    if(i<10){
+        numbers(i+1);
+    }
+}
+numbers(0);
+
+let nums = [0, 1, 2, 3, 4];
+
+function print(num){
+    console.log(num);
 }
 
-// let minutes = new date().getMinutes();
-// console.log(minutes);
-// while(minutes+1 != new Date().getMinutes()) {
-//     console.log('old minute');
-// }
-// console.log('new minute has started');
-let i = 1;
-while(i>10) {
-    console.log('while ' + i);
-}
-
-i = 1;
-do {
-    console.log('do ' + i);
-} while(i>10);
-
-let fruits = ['apple', 'cherry', 'strawberry', 'pear', 'melon'];
-
-for(let i = 0; i<fruits.length; i++); {
-    console.log(fruits[i]);
-}
-
-fruits.forEach(function (fruit){
-    console.log(fruit);
+nums.forEach(print);
+nums.forEach(function (num){
+    console.log('Num is ' + num);
 });
 
+nums.forEach((num) => {
+    console.log('Num is ' + num);
+});
+
+nums.forEach(num => {
+    console.log('Num is ' + num);
+});
+nums.forEach(num => console.log('Num is ' + num));
+
+let sums = nums.map(num => {
+    return num + 10;
+});
+console.log(sums);
+
+sums = nums.map(num => num + 10);
+console.log(sums);
+
+let sum = nums.reduce((total, num) => total+num);
+console.log(sums);
+
+let sorted = nums.sort((a,b) => {
+    if(a > b) {
+        return 1;
+    } else if(a < b) {
+        return -1;
+    } else {
+        return 0;
+    }
+});
+console.log(sorted);
+
+console.log(Math.PI);
+console.log(Math.round(Math.random() *50 ) + 50);
