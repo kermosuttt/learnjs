c3b009532716855299e5fa5c647d270e1b0de2f2
Kermo Sutt <kermo.sutt@tptlive.ee>
2023-09-25 10:24:49 +0300

    flag, star and circle with cut piece added

--- script.js
+++ script.js
@@ -4,16 +4,49 @@
 // }, 5000);
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
+
+ctx.rect(10, 10, 100, 90)
+ctx.strokeStyle = 'black';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+ctx.fillStyle = '#0000FF';
+ctx.fillRect(10, 10, 100, 30);
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(10, 40, 100, 30);
+
+ctx.fillStyle = '#FFFFFF';
+ctx.fillRect(10, 70, 100, 30);
+
+ctx.moveTo(200,20);
+ctx.lineTo(160,50);
 ctx.stroke();
 ctx.beginPath();
-ctx.arc(100,100, 50, 0, 2 * Math.PI);
-ctx.strokeStyle = 'red'
-ctx.lineWidth = 3;
+
+ctx.moveTo(150,20);
+ctx.lineTo(190,50);
 ctx.stroke();
-ctx.font = '30px Arial';
-ctx.fillText('Hello Canvas', 30, 50);
-ctx.strokeText('Hello Canvas', 30, 100);
-ctx.fillStyle = '#ff0';
-ctx.fillRect(10, 10, 10, 10);
+
+ctx.moveTo(150,20);
+ctx.lineTo(200,20);
+ctx.stroke();
+
+ctx.moveTo(175,5);
+ctx.lineTo(160,50);
+ctx.stroke();
+
+ctx.moveTo(175,5);
+ctx.lineTo(190,50);
+ctx.stroke();
+
+ctx.moveTo(290,80);
+ctx.arc(250, 80, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.lineTo(250,80);
+ctx.arc(250, 80, 40, 4.2, -3.5 / Math.PI);
+ctx.fillStyle = 'black';
+ctx.fill();
+ctx.stroke();
